71accc8b22abfb62606f460fe165673e1c500ff4
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Pro.local>
2021-06-21 19:05:36 -0700

    add routes and auth

---
+++ views/admin/auth/signup.js
@@ -0,0 +1,17 @@
+const layout = require('../layout');
+
+module.exports = ({ req }) => {
+  return layout({
+    content: `
+        <div>
+          Your id is: ${req.session.userId}
+          <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confimation" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+    `
+  })
+}
