3764fc42e715db11b845a1ab4ee9f4bb9faab2c0
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Pro.local>
2021-07-08 21:36:26 -0700

    add cart add and cart delete

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -23,8 +25,35 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items
   });
 
-  res.send('Product added to cart');
+  res.redirect('/cart');
 })
+
+router.get('/cart', async (req, res) => {
+  if(!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items){
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({items: cart.items}));
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+  const {itemId} = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter(item => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect('/cart');
+});
 
 
 module.exports = router;
